5c46b5949c8fde166409944832d4399b15222622
AhteshamulHaque <ahteshamul8900@gmail.com>
2020-04-17 17:29:07 +0530

    Added dynamic chart

--- user/views.py
+++ user/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+
 # Create your views here.
 
 def home(request):
 	employees = Employee.objects.all()
-
-	return render(request, 'user/main.html', {'employees':employees})
+	# chart data
+	data = [
+         ['Year', 'Sales', 'Growth', 'Profit'],
+         ['2014', 500, 400, 200],
+         ['2015', 120, 460, 250],
+      	['2016', 660, 120, 300],
+         ['2017', 130, 540, 350]
+      ]
+ 
+	return render(request, 'user/main.html', {'employees':employees, 'data': data })
